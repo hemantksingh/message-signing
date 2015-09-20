6efdc38d2287ddc15e4b14cac2d6a8e46d489574
hemantksingh <hemantksingh.hk@gmail.com>
2015-09-20 19:44:34 +0100

    Adds test for building canonical message.

--- src/test/java/com/inivaran/messagesigning/VerifySignedMessage.java
+++ src/test/java/com/inivaran/messagesigning/VerifySignedMessage.java
@@ -2,26 +2,33 @@ package com.inivaran.messagesigning;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.util.Collections.unmodifiableMap;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
 public class VerifySignedMessage {
 
     @Test
-    public void messageSignedWithValidSignatureIsVerified() throws KeyLoaderException, MessageSigningException {
+    public void messageSignedWithValidSignatureIsVerified()
+            throws KeyLoaderException, MessageSigningException, URISyntaxException {
         XBarSignature signature = signMessage();
-        System.out.print(signature.metadata);
 
         String requestMethod = "POST";
         String requestPath = "https://sandbox.inivaran.com";
         String requestBody = "a=ab&b=cd";
-        Map<String, String> requestHeaders = Collections.unmodifiableMap(new HashMap<String, String>() {
+        Map<String, String> requestHeaders = unmodifiableMap(new HashMap<String, String>() {
             {
                 put("CONTENT-TYPE", "application/x-www-form-urlencoded");
                 put("X-Bar-Signature-Metadata", signature.metadata);
@@ -29,7 +36,8 @@ public class VerifySignedMessage {
             }
         });
 
-        PublicKey publicKey = new RSAKeyLoader().loadPublicKey("src/test/resources/public_key.der");
+        String filename = getResourcePath("public_key.der").toString();
+        PublicKey publicKey = new RSAKeyLoader().loadPublicKey(filename);
 
         VerificationResult verificationResult = new RESTMessageSigning().verifyRequest(
                 publicKey,
@@ -38,14 +46,47 @@ public class VerifySignedMessage {
         assertTrue(verificationResult.verified);
     }
 
-    private XBarSignature signMessage() throws KeyLoaderException, MessageSigningException {
+
+    @Test
+    public void canonicalMessageIsInExpectedFormat() throws URISyntaxException, IOException {
+
+        Map<String, String> requestHeaders = unmodifiableMap(new HashMap<String, String>() {
+            {
+                put("CONTENT-TYPE", "application/x-www-form-urlencoded");
+                put("X-BAR-SIGNATURE-METADATA", CanonicalMessage.buildSignatureMetadata(
+                        "external-client",
+                        "https://sandbox.inivaran.com",
+                        5,
+                        144077601900L));
+            }
+        });
+
+        String canonicalRequest = CanonicalMessage.buildCanonicalRequest(
+                "POST",
+                "https://sandbox.inivaran.com",
+                "a=ab&b=cd",
+                requestHeaders);
+
+        String expected = new String(Files.readAllBytes(getResourcePath("ExpectedCanonicalForm.txt")));
+        assertEquals(expected, canonicalRequest);
+    }
+
+    private static Path getResourcePath(String resourceName) throws URISyntaxException {
+        URL resource = ClassLoader.getSystemClassLoader().getResource(resourceName);
+
+        if(resource != null)
+            return Paths.get(resource.toURI());
+        return Paths.get("");
+    }
+
+    private XBarSignature signMessage() throws KeyLoaderException, MessageSigningException, URISyntaxException {
         String clientId = "external-client";
         Long requestTimestamp = 144077601900L;
         Integer requestId = 5;
         String requestMethod = "POST";
         String requestPath = "https://sandbox.inivaran.com";
         String requestBody = "a=ab&b=cd";
-        Map<String, String> requestHeaders = Collections.unmodifiableMap(new HashMap<String, String>() {
+        Map<String, String> requestHeaders = unmodifiableMap(new HashMap<String, String>() {
             {
                 put("CONTENT-TYPE", "application/x-www-form-urlencoded");
             }
@@ -59,7 +100,8 @@ public class VerifySignedMessage {
                 requestTimestamp,
                 requestId);
 
-        PrivateKey privateKey = new RSAKeyLoader().loadPrivateKey("src/test/resources/private_key.der");
+        String filename = getResourcePath("private_key.der").toString();
+        PrivateKey privateKey = new RSAKeyLoader().loadPrivateKey(filename);
 
         return new RESTMessageSigning().signRequest(privateKey, detail);
     }
